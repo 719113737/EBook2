d2cb2eab8b77d1faa3dcde8075a985bec5b23deb
86188 <18301063@bjtu.edu.cn>
2021-05-21 19:09:07 +0800

    test

---
+++ src/test/java/com/example/demo/controller/BookControllerTest.java
@@ -0,0 +1,37 @@
+package com.example.demo.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BookControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getBooks() {
+    }
+
+    @Test
+    public void getBookByTitle() {
+    }
+
+    @Test
+    public void getCollectionByUsername() {
+    }
+
+    @Test
+    public void getCollection() {
+    }
+
+    @Test
+    public void addCollection() {
+    }
+
+    @Test
+    public void deleteCollection() {
+    }
+}
---
+++ src/test/java/com/example/demo/controller/UserControllerTest.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void register() {
+    }
+
+    @Test
+    public void getInfoByUsername() {
+    }
+
+    @Test
+    public void modifyUserPhone() {
+    }
+}
